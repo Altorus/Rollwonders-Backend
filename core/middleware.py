44b4359f6dc52a4dd831972c60c91f4f0465f541
Aleksandrov Ivan <101462143+Altorus@users.noreply.github.com>
2024-09-11 20:20:14 +0400

    future: add current time zone

--- core/middleware.py
+++ core/middleware.py
@@ -1,4 +1,6 @@
 from . import constants
+import zoneinfo
+from django.utils import timezone
 
 
 class UTMSaveMiddleware:
@@ -19,3 +21,21 @@ class UTMSaveMiddleware:
 
         response = self.get_response(request)
         return response
+
+
+class TimezoneMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        try:
+            # get django_timezone from cookie
+            tzname = request.COOKIES.get("django_timezone")
+            if tzname:
+                timezone.activate(zoneinfo.ZoneInfo(tzname))
+            else:
+                timezone.deactivate()
+        except Exception as e:
+            timezone.deactivate()
+
+        return self.get_response(request)
