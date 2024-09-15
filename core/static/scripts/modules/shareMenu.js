76ccf8e97c13f5b8c05831c46d228fc9b4c36eb8
Aleksandrov Ivan <101462143+Altorus@users.noreply.github.com>
2024-09-15 19:19:17 +0400

    misc

--- core/static/scripts/modules/shareMenu.js
+++ core/static/scripts/modules/shareMenu.js
@@ -2,10 +2,12 @@ export function shareMenu() {
     const button = document.querySelector('.__openShareMenu');
     if (button) {
         button.addEventListener('click', (e) => {
+            const recipeName = document.querySelector('.__recipeName').innerText;
+
             if (navigator.share) {
                 navigator.share({
-                    title: "Поделиться рецептом",
-                    text: "Поделиться рецептом",
+                    title: recipeName,
+                    text: "Сочные рецепты в сервисе Rollwonders",
                     url: window.location.href
                 })
                     .then(() => console.log('Successful share'))
